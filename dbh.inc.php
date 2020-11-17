b23fd2cdbbab2a11ba07d95c6fe4de2b2ef91aae
Jonah Galvin <60234353+jhgalvin@users.noreply.github.com>
2020-11-17 16:41:05 -0600

    Add files via upload

---
+++ dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "pwdpwd";
+$dbName = "adminportaldb";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+if (!$conn) { //if didn't connect
+    die("Connection failed: ".mysqli_connect_error());
+}
